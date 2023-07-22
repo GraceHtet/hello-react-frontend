761927acca5b365d73260a34b372a7aaca05851c
Thiri <thirihtet246@gmail.com>
2023-07-22 12:06:43 +0630

    set up routes for greeting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router';
+import Greeting from './component/Greeting';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
