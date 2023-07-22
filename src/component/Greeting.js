3e7f137ab627eb4f5bb64d171b618e25d5d4d912
Thiri <thirihtet246@gmail.com>
2023-07-22 14:33:42 +0630

    Add reducer

--- src/component/Greeting.js
+++ src/component/Greeting.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { fetchGreetings } from '../store/greetingsSlice';
 
 const Greeting = () => {
-  return (
-    <div>
-      <h1>Greeting</h1>
-    </div>
-  );
+  const [number, setNumber] = useState(1); // [1
+  const { greetings, isLoading } = useSelector((state) => state.greetings);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchGreetings());
+    setNumber(Math.floor(Math.random() * 4));
+  }, []);
+
+  let message;
+  greetings.forEach((greeting, idx) => {
+    if (idx === number) message = greeting.message;
+  });
+
+  return <>{!isLoading && <h1>{message}</h1>}</>;
 };
 
 export default Greeting;
